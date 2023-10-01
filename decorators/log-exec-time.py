d22a80b382e307e3e0281d2fd5ce83f7351a29e0
Iaroslav Vorozhko <vorozhko@gmail.com>
2023-10-01 10:37:53 +0200

    Update log-exec-time.py

--- decorators/log-exec-time.py
+++ decorators/log-exec-time.py
@@ -1,6 +1,7 @@
-# Decorator to log executim time of a function
+# Decorator to log execution time of a function
 
 import time
+
 
 def log_exec_time(func):
     def wrapper():
@@ -10,11 +11,13 @@ def log_exec_time(func):
         print("seconds passed %0.4f" % ((end-start)/1000000000))
     return wrapper
 
-def do_smth():
+
+def do_something():
     cnt = 0
     for a in range(0, 1000000):
         cnt += a
-    
 
-do_smth = log_exec_time(do_smth)
-do_smth()
+
+# decorate do_something function
+do_something = log_exec_time(do_something)
+do_something()
