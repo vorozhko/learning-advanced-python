b0c39b17f1c6fe4ce65f56aa918f20d758f7ee0d
BuildTools <unconfigured@null.spigotmc.org>
2023-10-14 14:05:15 +0200

    Added threading example

---
+++ concurrency/concurrency.py
@@ -0,0 +1,33 @@
+import logging
+import threading
+import time
+import concurrent.futures
+
+
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    # Managing threads with ThreadPoolExecutor
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+        executor.map(thread_function, range(3))
+
+    # Making and managing threads with threading module
+    threads = list()
+    for index in range(3):
+        logging.info("Main    : create and start thread %d.", index)
+        x = threading.Thread(target=thread_function, args=(index,))
+        threads.append(x)
+        x.start()
+
+    for index, thread in enumerate(threads):
+        logging.info("Main    : before joining thread %d.", index)
+        thread.join()
+        logging.info("Main    : thread %d done", index)
